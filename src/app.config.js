ba42a2568936b384625d56d3555ed1d0b25ccc8d
曾威 <zengw02@mingyuanyun.com>
2021-12-26 12:01:13 +0800

    feat: 初始化项目(eslint 校验)

---
+++ src/app.config.js
@@ -0,0 +1,32 @@
+export default {
+  pages: [
+    'pages/index/index',
+    'pages/order/order',
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: '远方',
+    navigationBarTextStyle: 'black'
+  },
+  "tabBar": {
+    "color": "#7F8389",
+    "selectedColor": "#07c160",
+    "borderStyle": "black",
+    "backgroundColor": "#fff",
+    "list": [
+      {
+        "pagePath": "pages/index/index",
+        "iconPath": "assets/images/index-unselected.png",
+        "selectedIconPath": "assets/images/index-selected.png",
+        "text": "首页"
+      },
+      {
+        "pagePath": "pages/order/order",
+        "iconPath": "assets/images/order-unselected.png",
+        "selectedIconPath": "assets/images/order-selected.png",
+        "text": "我的订单"
+      }
+    ]
+  },
+}
---
+++ src/pages/order/order.jsx
@@ -0,0 +1,24 @@
+import { Component } from 'react'
+import { View, Text } from '@tarojs/components'
+import './order.scss'
+
+export default class Index extends Component {
+
+  componentWillMount () { }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className="order">
+        <Text>我的订单111</Text>
+      </View>
+    )
+  }
+}
